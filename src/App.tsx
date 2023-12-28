144cd11bf72c2ac23e2ff57aec6b572a42419c6a
Gabriel Zoneti <gabriel.zoneti@gmail.com>
2023-12-28 07:08:42 -0300

    Resolução troca CSS por styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
+import { ThemeProvider } from 'styled-components'
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
 import './global.css'
+import EstiloGlobal, { Container } from './styles'
+import temaPrincipal from './themes/temaPrincipal'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={temaPrincipal}>
+      <EstiloGlobal />
       <Header />
       <Hero />
-      <div className="container">
+      <Container>
         <ListaVagas />
-      </div>
-    </>
+      </Container>
+    </ThemeProvider>
   )
 }
 
---
+++ src/components/Cabecalho/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const CabecalhoHeader = styled.header`
+  background-color: ${(props) => props.theme.corSecundaria};
+  color: ${(props) => props.theme.corPrincipal};
+  text-align: center;
+  padding: 24px 0;
+`
+
+export default CabecalhoHeader
